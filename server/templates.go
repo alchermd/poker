c1ef1ee5774341d87e0df000f7291f2100e28003
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-06-05 12:24:13 +0800

    Create base layout with Bootstrap framework

--- server/templates.go
+++ server/templates.go
@@ -3,9 +3,11 @@ package server
 import "html/template"
 
 const tplRoot = "templates"
+const tplBase = "base.gohtml"
 
 func (s *Server) ParseTemplates() {
 	templateNames := []string{
+		tplRoot + "/" + tplBase,
 		tplRoot + "/" + tplHome,
 	}
 	s.templates = template.Must(template.ParseFiles(templateNames...))
