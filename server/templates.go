217617effad0277f2128f360380756860d91c8f9
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-06-06 22:29:51 +0800

    Add navbar

--- server/templates.go
+++ server/templates.go
@@ -2,13 +2,22 @@ package server
 
 import "html/template"
 
+// Base template
 const tplRoot = "templates"
 const tplBase = "base.gohtml"
 
+// Partial templates
+const tplPartialRoot = tplRoot + "/partials"
+const tplPartialNavbar = "navbar.gohtml"
+
 func (s *Server) ParseTemplates() {
-	templateNames := []string{
+	tplPartials := []string{
+		tplPartialRoot + "/" + tplPartialNavbar,
+	}
+	tpls := []string{
 		tplRoot + "/" + tplBase,
 		tplRoot + "/" + tplHome,
 	}
-	s.templates = template.Must(template.ParseFiles(templateNames...))
+	tpls = append(tplPartials, tpls...)
+	s.templates = template.Must(template.ParseFiles(tpls...))
 }
