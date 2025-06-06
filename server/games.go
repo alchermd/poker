d69096eb4ea6fa154a41c5561026776dd39c215f
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-06-07 02:32:03 +0800

    Split out GET and POST handler

--- server/games.go
+++ server/games.go
@@ -5,8 +5,14 @@ import "net/http"
 const tplCreateGame = "create-game.gohtml"
 
 func (s *Server) CreateGame(w http.ResponseWriter, r *http.Request) {
-	err := s.templates[tplCreateGame].ExecuteTemplate(w, tplCreateGame, nil)
-	if err != nil {
-		s.l.Printf("Error writing response %q", err)
+	if r.Method == http.MethodGet {
+		err := s.templates[tplCreateGame].ExecuteTemplate(w, tplCreateGame, nil)
+		if err != nil {
+			s.l.Printf("Error writing response %q", err)
+		}
+		return
+	} else if r.Method == http.MethodPost {
+		http.Error(w, "TODO: Implement me!", http.StatusNotImplemented)
+		return
 	}
 }
