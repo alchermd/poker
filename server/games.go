284ddda12f62a9de1c05a2ee3a8056c72ff9a44f
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2025-06-07 10:53:48 +0800

    Parse form data and redirect to game details after

--- server/games.go
+++ server/games.go
@@ -1,9 +1,19 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+)
 
 const tplCreateGame = "create-game.gohtml"
 
+// Think -- should this be defined in the server layer or in the core layer?
+type createGamePayload struct {
+	name        string
+	description string
+	players     []string
+}
+
+// CreateGame serves the HTML form for creating a game and the POST handling of the submitted data.
 func (s *Server) CreateGame(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		err := s.templates[tplCreateGame].ExecuteTemplate(w, tplCreateGame, nil)
@@ -12,7 +22,22 @@ func (s *Server) CreateGame(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	} else if r.Method == http.MethodPost {
-		http.Error(w, "TODO: Implement me!", http.StatusNotImplemented)
+		err := r.ParseForm()
+		if err != nil {
+			s.l.Printf("Error parsing form %q", err)
+			http.Error(w, "Error parsing form", http.StatusBadRequest)
+			return
+		}
+
+		// TODO: Pass this to a service function to facilitate game creation.
+		payload := &createGamePayload{
+			name:        r.Form.Get("name"),
+			description: r.Form.Get("description"),
+			players:     r.Form["players"],
+		}
+		s.l.Printf("Parsed payload: %#v", payload)
+
+		http.Redirect(w, r, "/games/1", http.StatusFound)
 		return
 	}
 }
